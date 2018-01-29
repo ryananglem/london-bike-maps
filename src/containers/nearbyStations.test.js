d5bbe0c1ddde217d626454192c1e10afa3db4b86
Ryan Anglem <ryan.anglem@gmail.com>
2018-01-29 12:32:10 +0000

    Updates

--- src/containers/nearbyStations.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import NearbyStations from './nearbyStations';
-import {shallow} from 'enzyme';
-
-it('should render nearby stations container', () => {
-    const wrapper= shallow(<NearbyStations />);
-
-});
