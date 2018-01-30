738aaad224fc8db5462646d6b852fc9d39350860
Ryan Anglem <ryan.anglem@gmail.com>
2018-01-30 17:20:10 +0000

    chore(): add menu component back to test

--- src/components/menu.test.js
+++ src/components/menu.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import {shallow} from 'enzyme';
 import ShallowRenderer from 'react-test-renderer/shallow';
- import Menu from './menu';
+import Menu from './menu';
 
 it('renders menu without crashing', () => {
     // const div = document.createElement('div');
